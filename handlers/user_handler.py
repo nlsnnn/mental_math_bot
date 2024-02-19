90714a268131693569c1987b1116a25100357655
nlsnnn <lagged.06@mail.ru>
2024-02-19 22:46:16 +0300

    add test start math handler

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -3,13 +3,16 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 from lexicon.lexicon import LEXICON_RU
 from keyboards.inline_keyboard import get_markup
+from aiogram.exceptions import TelegramBadRequest
+from time import sleep
+from services.services import generate_numbers
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
 async def start_cmd(message: Message):
-    markup = get_markup(3, 'start_btn', 'settings_btn', 'info_btn')
+    markup = get_markup(2, 'start_btn', 'settings_btn', 'info_btn')
     await message.answer(text=LEXICON_RU['/start'], reply_markup=markup)
 
 
@@ -25,9 +28,35 @@ async def settings_cmd(callback: CallbackQuery):
     await callback.message.edit_text(text=LEXICON_RU['settings'], reply_markup=markup)
 
 
+@user_router.callback_query(F.data == "start_btn")
+async def start_math(callback: CallbackQuery):
+    markup = get_markup(1, 'backward')
+    digits = generate_numbers(10)
+    for i in range(5, 0, -1):
+        await callback.message.edit_text(
+            text=f"<b>Начало через {i}...</b>"
+        )
+        sleep(0.5)
+
+    for number in digits:
+        try:
+            await callback.message.edit_text(
+                text=f"<b>{number}</b>"
+            )
+            sleep(1)
+        except TelegramBadRequest:
+            continue
+
+    await callback.message.edit_text(
+        text=f"<b>Ответ: {sum(digits)}</b>",
+        reply_markup=markup
+    )
+
+
+
 @user_router.callback_query(F.data == 'backward')
 async def backward(callback: CallbackQuery):
-    markup = get_markup(3, 'start_btn', 'settings_btn', 'info_btn')
+    markup = get_markup(2, 'start_btn', 'settings_btn', 'info_btn')
     await callback.message.edit_text(text=LEXICON_RU['/start'], reply_markup=markup)
 
 
